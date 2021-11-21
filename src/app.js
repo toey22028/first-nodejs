5d4cd3942242fade74f5d60e8e9f447d6088fc47
toey22028 <siripornt62@nu.ac.th>
2021-11-21 15:31:15 +0700

    dit

--- src/app.js
+++ src/app.js
@@ -7,18 +7,41 @@ app.use(express.urlencoded({extended: true}));
 
 require('./routes')(app)
 
-// app.get('/status', function(req,res){
-//     res.send('Hello nodejs server');
-// })
+app.put('/plus/:number', function(req,res){
 
-// app.get('/hello/:person', function(req,res){
-//     console.log('hello -'+req.params.person);
-//     res.send('say hello with ' + req.params.person);
-// })
+    let number = 5;
+    console.log(number+1+req.params.person);
+    res.send('Number is' + req.params.person);
 
+})
 
+app.put('/fac/:number', function(req,res){
+
+    let number = 5;
+    console.log(120+req.params.person);
+    res.send('Number is' + req.params.person);
+
+})
+
+app.put('/min/:number', function(req,res){
+
+    let number = 5;
+    console.log(number-1+req.params.person);
+    res.send('Number is' + req.params.person);
+
+})
+
+app.put('/fac/:number', function(req,res){
+
+    let number = 5;
+    console.log(120+req.params.person);
+    res.send('Number is' + req.params.person);
+
+})
 
 let port = 8081;
+
 app.listen(port, function(){
     console.log('server running on ' + port);
+    
 })
